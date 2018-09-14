7d624f5c6d6c37a0e5e0b6332a74862477fbc6aa
jongsukchoi <jjongssuk5@gmail.com>
2018-09-14 14:21:54 +0900

    modify

--- 01.basic_prac/tensorflow_basic_1.py
+++ 01.basic_prac/tensorflow_basic_1.py
@@ -7,9 +7,10 @@ print(hello)
 a = tf.constant(10)
 b = tf.constant(20)
 c = tf.add(a,b)
+d = a + b
 print(c)
 
 sess = tf.Session()
-print(sess.run(hello), sess.run([a,b,c]))
+print(sess.run(hello), sess.run([a,b,c,d]))
 
 sess.close()
